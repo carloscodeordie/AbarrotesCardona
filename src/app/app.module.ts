e3252ad490f6f937e9c331aa3893eab8dd762b81
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-12-21 22:37:16 -0600

    Add initial layout
    Add initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductDashboardComponent } from './components/product-dashboard/product-dashboard.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ProductsComponent,
     ProductDetailComponent,
     MessagesComponent,
-    ProductDashboardComponent
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +39,15 @@ import { MatSliderModule } from '@angular/material/slider';
     MatSliderModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
