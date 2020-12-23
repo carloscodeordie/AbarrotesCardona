34e11ab25372abda7f5ee1b4dddac10d9d075337
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-12-23 12:28:44 -0600

    Fix issue in filters for Product Dashboard page
    Fix issue in filters for Product Dashboard page

--- src/app/components/products-table/products-table.component.ts
+++ src/app/components/products-table/products-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MatSelectChange } from '@angular/material/select';
 
+import { MatSelectChange } from '@angular/material/select';
 import {MatTableDataSource} from '@angular/material/table';
 
 import { Product } from '../../interfaces/Product';
@@ -17,9 +17,12 @@ export class ProductsTableComponent implements OnInit {
 
   displayedColumns!: string[];
   dataSource!: any;
+  selectedFilter: string;
   selectedProvider: string;
 
   ngOnInit() {
+    this.selectedFilter = '';
+
     this.displayedColumns = ['name', 'brand', 'price'];
     this.dataSource = new MatTableDataSource(this.products);
   }
@@ -27,11 +30,15 @@ export class ProductsTableComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    this.selectedProvider = null;
   }
 
   applySelectFilter(option: MatSelectChange) {
     this.selectedProvider = option.value;
     this.dataSource.filter = this.selectedProvider.trim().toLowerCase();
+
+    this.selectedFilter = null;
   }
 
   onSelect(product: Product): void {
