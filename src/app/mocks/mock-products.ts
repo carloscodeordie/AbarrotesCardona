bae954aca2f86d672b18fe791cf1071c9b97ca0a
Carlos Macias <lmaciasgonzalez@gmail.com>
2021-01-02 16:02:15 -0600

    Add missing fields when adding and editing a product
    Add missing fields when adding and editing a product

--- src/app/mocks/mock-products.ts
+++ src/app/mocks/mock-products.ts
@@ -14,5 +14,187 @@ export const PRODUCTS: Product[] = [
         id: 1,
         name: 'Don Paco'
       } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
+    },
+    { 
+      id: 1, 
+      code: 123456789012,
+      name: 'Axion',
+      description: 'Producto de limpieza',
+      current_stock: 10,
+      minimum_stock: 5,
+      price_buy: 10,
+      price_sale: 20,
+      provider: {
+        id: 1,
+        name: 'Don Paco'
+      } 
     }
   ];
